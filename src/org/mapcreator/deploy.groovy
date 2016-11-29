8b9c21bee13a82215bfb91a10b7022df99e25d8e
Sven <me@xoryo.nl>
2016-11-29 16:20:18 +0100

    Adding deploy class

---
+++ src/org/mapcreator/deploy.groovy
@@ -0,0 +1,91 @@
+package org.mapcreator;
+
+class Deploy {
+	private String deployPath
+	private String deployBase
+	private String key
+	private String host
+	private String user
+	private boolean debug
+
+	def init(String key, String host, String user, String base, String projectName, String deployName) {
+		if(base[-1] != '/') {
+			base += '/'
+		}
+
+		this.deployBase = sprintf('%s%s/%s/', [base, projectName, deployName])
+		this.deployPath = sprintf('%srevisions/jenkins-%s-${BUILD_NUMBER}-%s', [this.deployBase, shellUtils.getUnixEpoch(), shellUtils.getRevision()])
+		this.debug = false
+	}
+
+	def enableDebug() {
+		this.debug = true
+	}
+
+	def unStash() {
+		echo 'Unstashing'
+
+		step([$class: 'wsCleanup'])
+		unstash 'deployable'
+	}
+
+	def prepare(List prepend = [], List append = []) {
+		commands = []
+		commands += prepend
+
+		commands += sprintf('mkdir -pv %s %s/shared', [deployPath, deployBasePath])
+		commands += sprintf('cd %s', [deployPath])
+
+		commands += append
+
+		if(!this.debug) {
+			shellUtils.ssh(this.user, this.host, this.key, commands)
+		} else {
+			println 'Debug SSH: \n'
+			println 'User: ' + this.user
+			println 'Host: ' + this.host
+			println 'Key: ' + this.key
+			println 'Commands: \n' + commands.join('\n')
+		}
+	}
+
+	def copy() {
+		echo 'Copying files...'
+
+		if(!this.debug) {
+			shellUtils.scp(this.user, this.host, this.key, './*', deployPath + '/')			
+		} else {
+			println 'Debug SCP: \n'
+			println 'User: ' + this.user
+			println 'Host: ' + this.host
+			println 'Key: ' + this.key
+			println 'From: ' + './*'
+			println 'To: ' + deployPath + '/'
+		}
+	}	
+
+	def finish(List shared = [], List prepend = [], List append = []) {
+		finish = []
+		finish += prepend
+
+		for(item in shared) {
+            finish += sprintf('rm -rv %s/%s || true', [this.deployPath, item])
+			finish += sprintf('ln -sv %s/shared/%s %s/%s', [this.deployBase, item, this.deployPath, item])			
+		}
+
+		finish += sprintf('rm -v %s/current', [this.deployBase])
+ 		finish += sprintf('ln -svf %s %s/current', [this.deployPath, this.deployBase])		
+		
+		finish += append
+
+		if(!this.debug) {
+			shellUtils.ssh(this.user, this.host, this.key, commands)
+		} else {
+			println 'Debug SSH: \n'
+			println 'User: ' + this.user
+			println 'Host: ' + this.host
+			println 'Key: ' + this.key
+			println 'Commands: \n' + commands.join('\n')
+		}
+	}
+}
