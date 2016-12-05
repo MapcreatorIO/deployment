fa73d97b7a9c88c8d3aa8c25b30a9b57de571095
Sven <me@xoryo.nl>
2016-12-05 11:35:15 +0100

    Add deploy functions

--- src/org/mapcreator/DeployArt.groovy
+++ src/org/mapcreator/DeployArt.groovy
@@ -5,4 +5,43 @@ class DeployArt extends DeployPHP {
 	DeployArt(steps) {
 		super(steps)
 	}
+
+	@Override
+	def prepare(List prepend = [], List append = []) {
+		this.steps.echo 'Installing composer'
+		this.steps.sh 'composer install --quiet --no-dev'
+
+		List commands = []
+		commands += prepend
+
+		commands += sprintf('mkdir -pv %s %sshared', [this.path, this.base])
+		commands += sprintf('cd %s', [this.path])
+
+		commands += append
+
+		this.shell.ssh(commands)
+	}
+
+	@Override
+	def finish(List shared = [], List prepend = [], List append = []) {
+		List commands = []
+		commands += prepend
+
+		for(item in shared) {
+			commands += sprintf('rm -rv %s/%s || true', [this.path, item])
+			commands += sprintf('ln -sv %sshared/%s %s/%s', [this.base, item, this.path, item])
+		}
+		
+		commands += sprintf('cd %s', [this.path])
+		commands += 'php artisan migrate'
+		commands += 'php artisan route:cache'
+		commands += 'php artisan config:cache'
+
+		commands += sprintf('rm -v %scurrent', [this.base])
+		commands += sprintf('ln -svf %s %scurrent', [this.path, this.base])
+
+		commands += append
+
+		this.shell.ssh(commands)
+	}
 }
