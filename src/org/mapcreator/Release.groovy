bc96773597039f5b5d4abd0fb4033aec92e2ee7a
Sven <me@xoryo.nl>
2016-12-06 10:35:42 +0100

    Add code to manage releases from git

---
+++ src/org/mapcreator/Release.groovy
@@ -0,0 +1,23 @@
+package org.mapcreator
+
+import java.io.Serializable
+import org.kohsuke.github.GitHub
+
+class Release extends Serializable {
+	protected String username
+	protected String key
+
+	def init(username, key) {
+		this.username = username
+		this.key = key
+	}	
+
+	@NonCPS
+	def getLatest(repo) {
+		git = GitHub.connect(this.username, this.key)
+		repo = git.getRepository(repo)
+		release = repo.listReleases()[0]
+
+		return release.getTarballUrl()
+	}
+}
