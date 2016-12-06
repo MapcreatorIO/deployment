34209a7ffb1c33af685d56aaaa71234a56afbd7a
Sven <me@xoryo.nl>
2016-12-06 12:02:05 +0100

    Always return a string

--- vars/credentials.groovy
+++ vars/credentials.groovy
@@ -1,24 +1,24 @@
 def getPassword(id) { 
-    def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
-        com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials.class,
-        jenkins.model.Jenkins.instance
-    )
+	def creds = com.cloudbees.plugins.credentials.CredentialsProvider.lookupCredentials(
+		com.cloudbees.plugins.credentials.common.StandardUsernamePasswordCredentials.class,
+		jenkins.model.Jenkins.instance
+	)
 
-    def c = creds.findResult { it.id == id ? it : null }
+	def c = creds.findResult { it.id == id ? it : null }
 
-    if ( c ) {
-        println "found credential ${c.id} for username ${c.username}"
+	if ( c ) {
+		println "found credential ${c.id} for username ${c.username}"
 
-        def systemCredentialsProvider = jenkins.model.Jenkins.instance.getExtensionList(
-            'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
-            ).first()
+		def systemCredentialsProvider = jenkins.model.Jenkins.instance.getExtensionList(
+			'com.cloudbees.plugins.credentials.SystemCredentialsProvider'
+		).first()
 
-      def password = systemCredentialsProvider.credentials.first().password
-    } else {
-      println "could not find credential for ${id}"
-    }
-	
-	return password
+		def password = systemCredentialsProvider.credentials.first().password
+		return password
+	} else {
+		println "could not find credential for ${id}"
+		return ""
+	}		
 }
 
 def call(id) {
