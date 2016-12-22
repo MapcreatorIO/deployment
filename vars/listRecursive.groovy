db277e0a09a3b80ba10b92492e810efba1cb2dfd
Sven <me@xoryo.nl>
2016-12-22 11:01:27 +0800

    Add list recursive method

---
+++ vars/listRecursive.groovy
@@ -0,0 +1,19 @@
+def listRecursive(loc, ext) {
+	def files = []
+
+	for(file in loc.listFiles() {
+		if(file.isFile()) {
+			if(file.getName().endsWith(ext)) {
+				files << file.getName()
+			}
+		} else {
+			files += getFileS(file)
+		}
+	}
+
+	return files
+}
+
+def call(loc, ext) {
+	return listRecursive(loc, ext)
+}
