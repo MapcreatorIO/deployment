d92e6c261535bb15f551e0ecc07e74c74c806d33
Sven Thiessen <sv.thiessen@gmail.com>
2017-06-02 14:27:58 +0200

    Remove redundant code

--- src/org/mapcreator/Changes.groovy
+++ src/org/mapcreator/Changes.groovy
@@ -4,43 +4,29 @@ import java.io.Serializable
 import org.kohsuke.github.GitHub
 
 class Changes implements Serializable {
-    protected String username
-    protected String key
-    protected String repo
-    protected boolean debug
-    
-    def init(username, key, repo, debug = false) {
-        this.username = username
-        this.key = key
-        this.repo = repo
-        this.debug = debug
-    }
+    public String username
+    public String key
+    public String repo
 
     def log(String message) {
         if(debug) {
             println(message)
         }
     }
-    
+
     @NonCPS
     def getByCommit(sha) {
-        this.log("Connecting to github...")
-
         def git = GitHub.connect(this.username, this.key)
         def repo = git.getRepository(this.repo)
-
-        this.log("Getting files from ${sha}")
         def changes = repo.getCommit(sha)
 
         def files = []
 
         for(item in changes.getFiles()) {
-            file = new GitFile(name: item.getFileName(), status: item.getStatus())
-            this.log("Adding item ${file.name}")
+            GitFile file = new GitFile(name: item.getFileName(), status: item.getStatus())
             files += file
         }
 
-        this.log("Returning items...")
         return files
     }
 }
