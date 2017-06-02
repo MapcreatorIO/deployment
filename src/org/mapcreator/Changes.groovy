55a9a63087f93c49c9d0a0fc0c7050582bb03c38
Sven Thiessen <sv.thiessen@gmail.com>
2017-06-02 13:36:46 +0200

    Added class to get changes from commit

---
+++ src/org/mapcreator/Changes.groovy
@@ -0,0 +1,36 @@
+package org.mapcreator
+
+import java.io.Serializable
+import org.kohsuke.github.GitHub
+
+class Changes implements Serializable {
+    protected String username
+    protected String key
+    protected String repo
+    
+    def init(username, key, repo) {
+        this.username = username
+        this.key = key
+        this.repo = repo
+    }
+    
+    @NonCPS
+    def getByCommit(sha) {
+        def git = Github.connect(this.username, this.key)
+        def repo = git.getRepository(this.repo)
+        
+        def files = []
+        
+        try {
+                def changes = repo.getCommit(sha)
+        } catch(e) {
+            return files
+        }
+        
+        for(file in changes.getFiles()) {
+            files += file.getFileName()
+        }
+        
+        return files
+    }
+}
