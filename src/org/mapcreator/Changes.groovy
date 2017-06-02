77547e688ab585c02b11c74876619d3cf98426ba
Sven Thiessen <sv.thiessen@gmail.com>
2017-06-02 13:59:24 +0200

    Add debug logging

--- src/org/mapcreator/Changes.groovy
+++ src/org/mapcreator/Changes.groovy
@@ -7,29 +7,43 @@ class Changes implements Serializable {
     protected String username
     protected String key
     protected String repo
+    protected boolean debug
     
-    def init(username, key, repo) {
+    def init(username, key, repo, debug = false) {
         this.username = username
         this.key = key
         this.repo = repo
+        this.debug = debug
+    }
+
+    def log(String message) {
+        if(!debug) return;
+
+
     }
     
     @NonCPS
     def getByCommit(sha) {
+        this.log("Connecting to github...")
+
         def git = GitHub.connect(this.username, this.key)
         def repo = git.getRepository(this.repo)
 
+        this.log("Getting files from ${sha}")
         def changes = repo.getCommit(sha)
 
         def files = []
 
         for(item in changes.getFiles()) {
+            this.log("Found item " + item.getFileName()+"...")
             def file = new GitFile()
+
             file.init(item.getFileName(), item.getStatus())
 
             files += file
         }
 
+        this.log("Returning items...")
         return files
     }
 }
