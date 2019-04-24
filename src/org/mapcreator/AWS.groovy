3f81b338a75014018698ac59b25db230a16efda9
xoryo <sven.thiessen@mapcreator.eu>
2019-04-24 10:26:48 +0200

    Split IP's into a list

--- src/org/mapcreator/AWS.groovy
+++ src/org/mapcreator/AWS.groovy
@@ -4,8 +4,8 @@ import java.io.Serializable
 
 class AWS implements Serializable {
 	def getIPS(id, steps) {
-		def command = "aws ec2 describe-instances --filters 'Name=tag:Identity,Values=${id}' 'Name=instance-state-code,Values=16' | jq '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp"
+		def command = "aws ec2 describe-instances --filters 'Name=tag:Identity,Values=${id}' 'Name=instance-state-code,Values=16' | jq '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp'"
 
-		return steps.sh(returnStdout: true, script: command).trim()
+		return steps.sh(returnStdout: true, script: command).trim().split('\n')
 	}
 }
