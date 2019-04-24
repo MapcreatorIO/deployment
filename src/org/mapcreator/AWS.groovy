b66df74c41625680705114c27d4cc248d6324d9b
xoryo <sven.thiessen@mapcreator.eu>
2019-04-24 10:21:48 +0200

    Make method CPS

--- src/org/mapcreator/AWS.groovy
+++ src/org/mapcreator/AWS.groovy
@@ -3,7 +3,6 @@ package org.mapcreator
 import java.io.Serializable
 
 class AWS implements Serializable {
-	@NonCPS
 	def getIPS(id, steps) {
 		def command = "aws ec2 describe-instances --filters 'Name=tag:Identity,Values=${id}' 'Name=instance-state-code,Values=16' | jq '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp"
 
