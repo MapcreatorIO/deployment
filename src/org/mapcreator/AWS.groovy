2e2c44213541711477b8b04dccebe00466257a0e
xoryo <sven.thiessen@mapcreator.eu>
2019-04-24 10:19:50 +0200

    Return output of method

--- src/org/mapcreator/AWS.groovy
+++ src/org/mapcreator/AWS.groovy
@@ -7,7 +7,6 @@ class AWS implements Serializable {
 	def getIPS(id, steps) {
 		def command = "aws ec2 describe-instances --filters 'Name=tag:Identity,Values=${id}' 'Name=instance-state-code,Values=16' | jq '.Reservations[].Instances[].NetworkInterfaces[].Association.PublicIp"
 
-		def output = steps.sh(returnStdout: true, script: command).trim()
-		steps.echo output
+		return steps.sh(returnStdout: true, script: command).trim()
 	}
 }
